9a8310c647f02916a6605018fdce3416c0458b54
Dinesh Magar <magard1010@gmail.com>
2024-10-05 00:54:54 +0545

    nested routes

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,5 @@
+
+
+export default function Blog() {
+    return <h1>Welcome Blog!!!</h1>
+}
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,5 @@
+
+
+export default function SecondBlog() {
+    return <h1>Welcome SecondBlog Post!!!</h1>
+}
