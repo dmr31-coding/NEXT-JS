bb258e44a4ccc1bfa2f3924e8068abfa2f9d21b3
Dinesh Magar <magard1010@gmail.com>
2024-10-08 01:12:54 +0545

    file colocation private folder route groups

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,4 @@
+
+export default function PrivateRouter() {
+    return <h1>Private Router: you can't see this in browser</h1>;
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+function BarChart() {
+    return <h1>Bar Chart</h1>;
+}
+// above function is not shown bcz it is not default export
+
+
+export default function Dashboard() {
+    return <h1>Dashboard</h1>;
+}
