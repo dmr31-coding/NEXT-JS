011ba923da1bd790daab159fa65762e180e8c32e
Dinesh Magar <magard1010@gmail.com>
2024-10-30 17:59:17 +0545

    title metadata

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { link } from "fs";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import "./style.css";
 
 const navLinks = [
     {name: "Register", href: "/register"},
@@ -13,11 +17,16 @@ export default function AuthLayout({
   }: {
     children: React.ReactNode
   }) {
+
+    const pathname = usePathname();
+
     return (
       <div>
         {navLinks.map((link) => {
+
+            const isActive = pathname.startsWith(link.href);
             return (
-                <Link href={link.href} key={link.name}>
+                <Link href={link.href} key={link.name} className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}>
                     {link.name}
                 </Link>
             )
--- src/app/products/[productid]/page.tsx
+++ src/app/products/[productid]/page.tsx
@@ -1,13 +1,21 @@
 import { Metadata } from "next";
+import { resolve } from "path";
 type Props = {
     params: {
         productid: string;
     };
 };
 
-export const generateMetadata = ({params}: Props): Metadata => {
+export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
+
+    const title = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`iphone ${params.productid}`)
+        }, 100)
+    })
+
     return {
-        title:`Product ${params.productid}`,
+        title:`Product ${title}`,
     };
 };
 
