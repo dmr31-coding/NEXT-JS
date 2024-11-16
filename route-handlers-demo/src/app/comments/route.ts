6d7ff531ae00724d078fdbb70237de5552f3056d
Dinesh Magar <magard1010@gmail.com>
2024-11-16 18:11:55 +0545

    query parameters

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -1,9 +1,13 @@
 //34. GET request
 
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET() {
-    return Response.json(comments);
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("query");
+    const fileterdComments = query ? comments.filter((comment) => comment.text.includes(query)) : comments;
+    return Response.json(fileterdComments);
 }
 
 
