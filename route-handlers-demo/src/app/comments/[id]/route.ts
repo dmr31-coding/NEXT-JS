180ecdb424bd1fec4da9626b871cfe42e4733b49
Dinesh Magar <magard1010@gmail.com>
2024-11-16 17:00:30 +0545

    PATCH request

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -1,3 +1,5 @@
+//36. dynamic route handlers
+
 import { comments } from "../data";
 
 export async function GET(_request: Request, { params }: {params: { id: string }}) {
@@ -5,4 +7,16 @@ export async function GET(_request: Request, { params }: {params: { id: string }
         (comment) => comment.id === parseInt(params.id)
     );
     return Response.json(comment);
+}
+
+
+// 36. PATCH request
+export async function PATCH(request: Request, { params }: { params: {id: string } } ) {
+    const body = await request.json();
+    const { text } = body;
+    const index = comments.findIndex(
+        (comment) => comment.id === parseInt(params.id)
+    );
+    comments[index].text = text;
+    return Response.json(comments[index]);
 }
