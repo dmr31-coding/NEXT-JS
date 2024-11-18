fe709f5db1a2e47ca3585e50fd407b624e9c5d7f
Dinesh Magar <magard1010@gmail.com>
2024-11-18 14:41:42 +0545

    cookies in routehandlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,18 +1,25 @@
 //33. route handlers
 
 import { type NextRequest } from "next/server"
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
     const requestHeaders = new Headers(request.headers);
     const headerList = headers()
+    const theme = request.cookies.get("theme");
+
+    (await cookies()).set("resultPerPage", "30")
 
     console.log(requestHeaders.get("Authorization"));
     console.log((await headerList).get("Authorization"));
+    console.log(theme)
+    console.log((await cookies()).get("resultPerPage"))
 
     return new Response("<h1>Profile API data...</h1>", {
         headers: {
             "Content-Type": "text/html",
+            //42. cookies in routehandler
+            "Set-Cookie": "theme=dark",
         },
     })
 }
