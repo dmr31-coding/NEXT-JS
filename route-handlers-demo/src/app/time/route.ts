c1d77a32988d495f88e09885887471526f766b35
Dinesh Magar <magard1010@gmail.com>
2024-11-19 18:55:01 +0545

    middleware

--- route-handlers-demo/src/app/time/route.ts
+++ route-handlers-demo/src/app/time/route.ts
@@ -1,3 +1,7 @@
+//43. caching in routehandlers
+
+export const  dynamic = "force-dynamic";
+
 export async function GET() {
     return Response.json({
         time: new Date().toLocaleTimeString(),
