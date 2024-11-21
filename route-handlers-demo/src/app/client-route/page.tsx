5ef8b685b9938579060af257bd06242a001ef8d5
Dinesh Magar <magard1010@gmail.com>
2024-11-21 17:51:19 +0545

    client-only package

--- route-handlers-demo/src/app/client-route/page.tsx
+++ route-handlers-demo/src/app/client-route/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { serverSideFunction } from "@/utils/server-utils"
+import { clientSideFunction } from "@/utils/client-utils";
 
 
 export default function ClientRoutePage() {
 
     console.log("Client route rendered")
-    const result = serverSideFunction()
+    // const result = serverSideFunction()
+    const result = clientSideFunction()
+    
 
     return (
         <>
--- route-handlers-demo/src/app/server-route/page.tsx
+++ route-handlers-demo/src/app/server-route/page.tsx
@@ -1,8 +1,11 @@
+import { clientSideFunction } from "@/utils/client-utils"
 import { serverSideFunction } from "@/utils/server-utils"
 
 export default function ServerRoutePage() {
     console.log("Server route rendered")
     const result = serverSideFunction()
+
+    const clientResult = clientSideFunction()// gives an error when client fn. is used in server code
     
     return (
     <>
